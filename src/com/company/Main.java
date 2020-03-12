d8fbda3a7eabfa41d701a8b4eec4d7fd71f2e750
mawenjie1234 <307446028@qq.com>
2020-03-12 10:41:41 +0800

    添加文件。

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        int a[] = new int[]{1,8,6,2,5,4,8,3,7};
+//        int maxArea = maxArea(a);
+//        System.out.println(maxArea);
+//        climbStairs(4);
+    }
+
+    public static int maxArea(int[] height) {
+        if (height.length < 1) {
+            return 0;
+        }
+        int maxArea = 0;
+        for (int i = 0, j = height.length - 1; i<j ; ){
+            int minHeight = height[i] < height[j] ? height[i++] : height[j--];
+            maxArea = Math.max(maxArea, (j - i + 1) * minHeight);
+        }
+        return maxArea;
+    }
+
+    // 爬楼梯
+    public static int climbStairs(int n) {
+        if(n < 3) {
+            return n;
+        }
+        int f1 = 1, f2 = 2,f3 = 3;
+        for (int i = 3; i <= n; i++) {
+            f3 = f1 + f2;
+            f1 = f2;
+            f2 = f3;
+        }
+        return f3;
+    }
+}
