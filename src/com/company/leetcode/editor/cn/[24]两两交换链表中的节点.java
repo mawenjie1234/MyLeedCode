e894d16254eb11a5c021dd82d4471d8f33f6cea9
mawenjie1234 <307446028@qq.com>
2020-03-23 15:31:13 +0800

    添加链表两两交换。

---
+++ src/com/company/leetcode/editor/cn/[24]两两交换链表中的节点.java
@@ -0,0 +1,60 @@
+//给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。 
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 
+//
+// 示例: 
+//
+// 给定 1->2->3->4, 你应该返回 2->1->4->3.
+// 
+// Related Topics 链表
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+/*
+    给定 1->2->3->4, 你应该返回 2->1->4->3.
+ */
+
+class Solution {
+
+//    public class ListNode {
+//     int val;
+//     ListNode next;
+//     ListNode(int x) { val = x; }
+//    }
+
+    public ListNode swapPairs(ListNode head) {
+
+    }
+
+//    public ListNode swapPairs(ListNode head) {
+//        if(head == null || head.next == null) {
+//            return head;
+//        }
+//        ListNode first = head.next;
+//        ListNode pre = null;
+//        while (head != null && head.next != null) {
+//            ListNode next = head.next;
+//            ListNode thread = head.next.next;
+//            next.next = head;
+//            head.next = thread;
+//            head = thread;
+//            if(pre != null) {
+//                pre.next = next;
+//            }
+//            pre = next.next;
+//        }
+//        return first;
+//    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
