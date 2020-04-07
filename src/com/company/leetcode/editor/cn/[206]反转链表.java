36f4758fbde862cd4bb9391e385342f5c26744ea
mawenjie1234 <307446028@qq.com>
2020-04-07 09:35:18 +0800

    添加翻转链表

--- src/com/company/leetcode/editor/cn/[206]反转链表.java
+++ src/com/company/leetcode/editor/cn/[206]反转链表.java
@@ -19,9 +19,38 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+///反转一个单链表。
+//
+//示例:
+//
+//输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL
+//进阶:
+//你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
 class Solution {
+
+//    public class ListNode{
+//        int val;
+//        ListNode next;
+//        ListNode(int x) {val = x;}
+//    }
+
     public ListNode reverseList(ListNode head) {
 
     }
-}
+
+
+//    public ListNode reverseList(ListNode head) {
+//        ListNode pre = null;
+//        ListNode current = head;
+//        while (current != null) {
+//            ListNode nextTemp = current.next;
+//            current.next = pre;
+//            pre = current;
+//            current = nextTemp;
+//        }
+//        return pre;
+//    }
+//}
 //leetcode submit region end(Prohibit modification and deletion)
