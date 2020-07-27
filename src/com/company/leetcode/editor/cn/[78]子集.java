1b2c49a8662d9a753d9cfaef3bbfc77faf9da66b
mawenjie1234 <307446028@qq.com>
2020-07-27 22:00:43 +0800

    更新子集

--- src/com/company/leetcode/editor/cn/[78]子集.java
+++ src/com/company/leetcode/editor/cn/[78]子集.java
@@ -21,12 +21,58 @@
 //// 👍 671 👎 0
 //
 //
+//import java.util.ArrayList;
 //import java.util.List;
 //
 ////leetcode submit region begin(Prohibit modification and deletion)
 //class Solution {
-//    public List<List<Integer>> subsets(int[] nums) {
 //
+////    public static void main(String[] args) {
+////        List<List<Integer>> res = new Solution().subsets(new int[]{1, 2, 3});
+////        for (List<Integer> curr : res) {
+////            for (Integer num : curr) {
+////                System.out.print(num);
+////            }
+////            System.out.println();
+////        }
+////    }
+//
+//    public List<List<Integer>> subsets(int[] nums) {
+//        List<List<Integer>> res = new ArrayList<>();
+//        help(nums, 0, res, new ArrayList<>());
+//        return res;
 //    }
+//
+//    public void help(int[] nums, int index, List<List<Integer>> res, List<Integer> current) {
+//        if(index == nums.length) {
+//            res.add(new ArrayList<>(current));
+//            return;
+//        }
+//
+//        help(nums, index +1, res, current);
+//        current.add(nums[index]);
+//        help(nums, index +1, res, current);
+//
+//        current.remove(current.size() -1);
+//    }
+//
+//
+//
+////    public List<List<Integer>> subsets(int[] nums) {
+////        List<List<Integer>> res = new ArrayList<>();
+////        res.add(new ArrayList<>());
+////        for (int num : nums) {
+////            List<List<Integer>> newSubsets = new ArrayList<>();
+////            for (List<Integer> curr : res) {
+////                ArrayList<Integer> resNew = new ArrayList<>(curr) {
+////                };
+////                resNew.add(num);
+////                newSubsets.add(resNew);
+////            }
+////            res.addAll(newSubsets);
+////        }
+////
+////        return res;
+////    }
 //}
 ////leetcode submit region end(Prohibit modification and deletion)
