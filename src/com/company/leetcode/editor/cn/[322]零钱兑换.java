7a5085d880febe1ae31bc8e6f4e26e2f6b93a97b
wenjie.ma <wenjie.ma@minifoxgame.com>
2020-08-13 09:54:08 +0800

    动态规划基础，打家劫舍

--- src/com/company/leetcode/editor/cn/[322]零钱兑换.java
+++ src/com/company/leetcode/editor/cn/[322]零钱兑换.java
@@ -33,29 +33,34 @@
 //        System.out.println(new Solution().coinChange(coins, 11));
 //    }
 //
-//
 //    public int coinChange(int[] coins, int amount) {
-//        if(amount < 1) return 0;
-//        return helper(coins, amount, new int[amount]);
+//
 //    }
 //
-//    private int helper(int[] coins, int rem, int[] count) {
-//        if(rem < 0) {
-//            return -1;
-//        }
-//        if(rem == 0) {
-//            return 0;
-//        }
-//        if(count[rem-1] != 0) return count[rem-1];
-//        int min = Integer.MAX_VALUE;
-//        for (int coin: coins) {
-//            int res = helper(coins, rem - coin, count);
-//            if(res >= 0 && res < min) {
-//                min = 1 + res;
-//            }
-//        }
-//        count[rem -1] = (min == Integer.MAX_VALUE) ? -1 : min;
-//        return count[rem -1];
-//    }
+//
+//
+////    public int coinChange(int[] coins, int amount) {
+////        if(amount < 1) return 0;
+////        return helper(coins, amount, new int[amount]);
+////    }
+////
+////    private int helper(int[] coins, int rem, int[] count) {
+////        if(rem < 0) {
+////            return -1;
+////        }
+////        if(rem == 0) {
+////            return 0;
+////        }
+////        if(count[rem-1] != 0) return count[rem-1];
+////        int min = Integer.MAX_VALUE;
+////        for (int coin: coins) {
+////            int res = helper(coins, rem - coin, count);
+////            if(res >= 0 && res < min) {
+////                min = 1 + res;
+////            }
+////        }
+////        count[rem -1] = (min == Integer.MAX_VALUE) ? -1 : min;
+////        return count[rem -1];
+////    }
 //}
 ////leetcode submit region end(Prohibit modification and deletion)
