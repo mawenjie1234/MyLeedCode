3a670ef98d94381ff9ce0031c04a94179b025e1b
mawenjie1234 <307446028@qq.com>
2020-08-21 22:21:10 +0800

    完成岛屿数量 使用并查集

--- src/com/company/leetcode/editor/cn/[200]岛屿数量.java
+++ src/com/company/leetcode/editor/cn/[200]岛屿数量.java
@@ -41,26 +41,70 @@
 //
 ////leetcode submit region begin(Prohibit modification and deletion)
 //class Solution {
-//    public static void main(String[] args) {
-//        char[][] grid = {
-//                {'1', '1', '1', '1', '1', '1', '1'},
-//                {'0', '0', '0', '0', '0', '0', '1'},
-//                {'1', '1', '1', '1', '1', '0', '1'},
-//                {'1', '0', '0', '0', '1', '0', '1'},
-//                {'1', '0', '1', '0', '1', '0', '1'},
-//                {'1', '0', '1', '1', '1', '0', '1'},
-//                {'1', '1', '1', '1', '1', '1', '1'},
+////    public static void main(String[] args) {
+////        char[][] grid = {
+////                {'1', '1', '1', '1', '1', '1', '1'},
+////                {'0', '0', '0', '0', '0', '0', '1'},
+////                {'1', '1', '1', '1', '1', '0', '1'},
+////                {'1', '0', '0', '0', '1', '0', '1'},
+////                {'1', '0', '1', '0', '1', '0', '1'},
+////                {'1', '0', '1', '1', '1', '0', '1'},
+////                {'1', '1', '1', '1', '1', '1', '1'},
+////
+////        };
+////        char[][] grid1 = {
+////                {'1', '1', '1', '1', '0'},
+////                {'1', '1', '0', '1', '0'},
+////                {'1', '1', '0', '0', '0'},
+////                {'0', '0', '0', '0', '0'}
+////        };
+////        char[][] grid2 = {
+////                {'1', '1', '0', '0', '0'},
+////                {'1', '1', '0', '0', '0'},
+////                {'0', '0', '1', '0', '0'},
+////                {'0', '0', '0', '1', '1'}
+////        };
+////
+//////        System.out.println(new Solution().numIslands1(grid2));
+////
+////        System.out.println(new Solution().numIslands(grid1));
+////    }
 //
-//        };
-//        System.out.println(new Solution().numIslands(grid));
-//    }
 //    public int numIslands(char[][] grid) {
-//        return 0;
+//        int row = grid.length;
+//        if (row == 0) {
+//            return 0;
+//        }
+//        int column = grid[0].length;
+//        if (column == 0) {
+//            return 0;
+//        }
+//        UnionFind find = new UnionFind(row * column + 1);
+//        int waterPoint = row * column;
+//
+//        for (int i = 0; i < row; i++) {
+//            for (int j = 0; j < column; j++) {
+//                if (grid[i][j] == '0') {
+//                    find.union(i * column + j, waterPoint);
+//                } else {
+//
+//                    if (i + 1 < row && grid[i + 1][j] == '1') {
+//                        find.union(i * column + j, (i + 1) * column + j);
+//                    }
+//                    if (j + 1 < column && grid[i][j + 1] == '1') {
+//                        find.union(i * column + j, i * column + j + 1);
+//                    }
+//                }
+//            }
+//        }
+//
+//        return find.count - 1;
 //    }
 //
 //    private class UnionFind {
 //        int[] parent;
 //        int count = 0;
+//
 //        public UnionFind(int n) {
 //            parent = new int[n];
 //            count = n;
@@ -69,51 +113,68 @@
 //            }
 //        }
 //
-//        
+//        public int findParent(int p) {
+//            while (p != parent[p]) {
+//                parent[p] = parent[parent[p]];
+//                p = parent[p];
+//            }
+//            return p;
+//        }
+//
+//        public void union(int p, int q) {
+//            int parentP = findParent(p);
+//            int parentQ = findParent(q);
+//            if (parentP == parentQ) {
+//                return;
+//            }
+//            parent[parentQ] = parentP;
+//            count--;
+//        }
+//
+//
 //    }
 //
 //
+//    public int numIslands1(char[][] grid) {
+//        if (grid == null || grid.length == 0) {
+//            return 0;
+//        }
+//        int nr = grid.length;
+//        int nc = grid[0].length;
+//        int count = 0;
 //
-////    public int numIslands(char[][] grid) {
-////        if (grid == null || grid.length == 0) {
-////            return 0;
-////        }
-////        int nr = grid.length;
-////        int nc = grid[0].length;
-////        int count = 0;
-////
-////        for (int r = 0; r < nr; r++) {
-////            for (int c = 0; c < nc; c++) {
-////                if (grid[r][c] == '1') {
-////                    count++;
-////                    grid[r][c] = '0';
-////                    Queue<Integer> neighbors = new LinkedList<>();
-////                    neighbors.add(r * nc + c);
-////                    while (!neighbors.isEmpty()) {
-////                        int id = neighbors.remove();
-////                        int row = id / nc;
-////                        int column = id % nc;
-////                        if (row - 1 >= 0 && grid[row - 1][column] == '1') { // top
-////                            neighbors.add((row - 1) * nc + column);
-////                            grid[row - 1][column] = '0';
-////                        }
-////                        if (row + 1 < nr && grid[row + 1][column] == '1') { // bottom
-////                            neighbors.add((row + 1) * nc + column);
-////                            grid[row + 1][column] = '0';
-////                        }
-////                        if (column - 1 >= 0 && grid[row][column - 1] == '1') { // left
-////                            neighbors.add(row * nc + column - 1);
-////                            grid[row][column - 1] = '0';
-////                        }
-////                        if (column + 1 < nc && grid[row][column + 1] == '1') { // right
-////                            neighbors.add(row * nc + column + 1);
-////                            grid[row][column + 1] = '0';
-////                        }
-////                    }
-////                }
-////            }
-////        }
-////        return count;
-////    }
+//        for (int r = 0; r < nr; r++) {
+//            for (int c = 0; c < nc; c++) {
+//                if (grid[r][c] == '1') {
+//                    count++;
+//                    grid[r][c] = '0';
+//                    Queue<Integer> neighbors = new LinkedList<>();
+//                    neighbors.add(r * nc + c);
+//                    while (!neighbors.isEmpty()) {
+//                        int id = neighbors.remove();
+//                        int row = id / nc;
+//                        int column = id % nc;
+//                        if (row - 1 >= 0 && grid[row - 1][column] == '1') { // top
+//                            neighbors.add((row - 1) * nc + column);
+//                            grid[row - 1][column] = '0';
+//                        }
+//                        if (row + 1 < nr && grid[row + 1][column] == '1') { // bottom
+//                            neighbors.add((row + 1) * nc + column);
+//                            grid[row + 1][column] = '0';
+//                        }
+//                        if (column - 1 >= 0 && grid[row][column - 1] == '1') { // left
+//                            neighbors.add(row * nc + column - 1);
+//                            grid[row][column - 1] = '0';
+//                        }
+//                        if (column + 1 < nc && grid[row][column + 1] == '1') { // right
+//                            neighbors.add(row * nc + column + 1);
+//                            grid[row][column + 1] = '0';
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        return count;
+//    }
 //}
 ////leetcode submit region end(Prohibit modification and deletion)
