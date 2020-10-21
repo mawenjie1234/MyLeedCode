6b2bf4a53536e608302d93d926d0a57d7ebe1a25
wenjie.ma <wenjie.ma@minifoxgame.com>
2020-10-21 10:09:09 +0800

    32 最长有效括号使用栈的方式，接下来还需要使用动态规划做一遍

--- src/com/company/leetcode/editor/cn/[32]最长有效括号.java
+++ src/com/company/leetcode/editor/cn/[32]最长有效括号.java
@@ -1,4 +1,4 @@
-////给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+//package com.company.leetcode.editor.cn;//给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
 ////
 //// 示例 1:
 ////
@@ -17,10 +17,69 @@
 //// 👍 951 👎 0
 //
 //
+//import java.util.Deque;
+//import java.util.LinkedList;
+//import java.util.Queue;
+//
 ////leetcode submit region begin(Prohibit modification and deletion)
 //class Solution {
-//    public int longestValidParentheses(String s) {
+////    public int longestValidParentheses(String s) {
+////
+////    }
 //
+////    public static void main(String[] args) {
+////        System.out.println(new Solution().longestValidParentheses("()()"));
+////        System.out.println(new Solution().longestValidParentheses(")()())"));
+////        System.out.println(new Solution().longestValidParentheses("()(()"));
+////    }
+//
+////    public int longestValidParentheses(String s) {
+////
+////    }
+//
+//
+//    public int longestValidParentheses(String s) {
+//        int max = 0;
+//        Deque<Integer> stack = new LinkedList<>();
+//        stack.add(-1);
+//        for (int i = 0; i < s.length(); i++) {
+//            if (s.charAt(i) == '(') {
+//                stack.push(i);
+//            } else {
+//                stack.pop();
+//                if (stack.isEmpty()) {
+//                    stack.push(i);
+//                } else {
+//                    max = Math.max(max, i - stack.peek());
+//                }
+//            }
+//        }
+//
+//        return max;
 //    }
+//
+//
+////    public int longestValidParentheses(String s) {
+////        Queue<Character> stack = new LinkedList<>();
+////        int count = 0;
+////        int lastCount = 0;
+////        for (int i = 0; i < s.length(); i++) {
+////            char ch = s.charAt(i);
+////            if (ch == '(') {
+////                stack.add(ch);
+////            } else if (ch == ')') {
+////                if (stack.size() > 0) {
+////                    stack.poll();
+////                    count += 2;
+////                }else {
+////                    lastCount = Math.max(count, lastCount);
+////                    count = 0;
+////                }
+////            } else {
+////                return Math.max(count, lastCount);
+////            }
+////        }
+////        return Math.max(count, lastCount);
+////    }
 //}
 ////leetcode submit region end(Prohibit modification and deletion)
